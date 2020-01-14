8bed1eb06bc926ab266f832badde60486c3ddeaf
akhil dhiman <dhimanakhil95@gmail.com>
2020-01-15 00:55:51 +0530

    add mongodb connection

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require("mongoose")
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +34,15 @@ if (process.env.NODE_ENV === "development") {
 
   app.use(require("webpack-hot-middleware")(compiler));
 }
+
+mongoose.connect("mongodb://localhost:27017/gratitudeJournalApp", {useNewUrlParser: true}, function(err) {
+  if (err) {
+    console.log("Not connected to the database")
+  } else {
+    console.log("Connected to the database")
+  } 
+})
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
