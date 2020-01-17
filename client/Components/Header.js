78192f761150c4605c73dec1cf4160dbe1ea8b1f
akhil dhiman <dhimanakhil95@gmail.com>
2020-01-17 15:47:49 +0530

    add header

---
+++ client/Components/Header.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <nav className="navbar is-link" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <h1 className="has-text-weight-semibold is-family-primary is-size-5 is-size-6-mobile is-family-sans-serif">Gratitude Journal App</h1>
+                </a>
+
+                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <div className="navbar-item has-dropdown is-hoverable">
+                    </div>
+                </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-sucess">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a className="button is-success">
+                                <strong>Log in</strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+export default Header
