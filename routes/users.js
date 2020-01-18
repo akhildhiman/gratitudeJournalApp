af70733399ca6876a91b090435355f5fb4fec8ee
akhil dhiman <akhilblackmetal69@gmail.com>
2020-01-18 12:24:27 +0530

    update emailController

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController")
-const emailController = require("../controllers/emailController")
+// const emailController = require("../controllers/emailController")
 
 router.post("/register", userController.registerUser)
 router.post("/login", userController.loginUser)
 router.get("/:userId", userController.getUser)
-router.post("/user/:email", emailController.sendPasswordResetEmail)
-router.post("/receive_new_password/:userId/:token", emailController.receiveNewPassword)
+// router.post("/user/:email", emailController.sendPasswordResetEmail)
+// router.post("/receive_new_password/:userId/:token", emailController.receiveNewPassword)
 
 
 module.exports = router;
--- utils/mailer.js
+++ utils/mailer.js
@@ -1,28 +1,28 @@
-const nodemailer = require("nodemailer")
+// const nodemailer = require("nodemailer")
 
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    }
-})
+// transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASSWORD
+//     }
+// })
 
-export const getPasswordResetURL = (user, token) => {
-    `http://localhost:3000/password/reset/${user._id}/${token}`
-}
+// getPasswordResetURL = (user, token) => {
+//     `http://localhost:3000/password/reset/${user._id}/${token}`
+// }
 
-export const resetPasswordTemplate = (user, url) => {
-    const from = process.env.EMAIL
-    const to = user.email
-    const subject = "Password Reset"
-    const html = `
-        <p>Hey ${user.name || user.email},</p>
-        <p>We heard that you forgot your password. Sorry about that!</p>
-        <p>But don’t worry! You can use the following link to reset your password:</p>
-        <a href=${url}>${url}</a>
-        <p>If you don’t use this link within 1 hour, it will expire.</p>
-    `
-}
+// resetPasswordTemplate = (user, url) => {
+//     const from = process.env.EMAIL
+//     const to = user.email
+//     const subject = "Password Reset"
+//     const html = `
+//         <p>Hey ${user.name || user.email},</p>
+//         <p>We heard that you forgot your password. Sorry about that!</p>
+//         <p>But don’t worry! You can use the following link to reset your password:</p>
+//         <a href=${url}>${url}</a>
+//         <p>If you don’t use this link within 1 hour, it will expire.</p>
+//     `
+// }
 
-return { from, to, subject, html }
+// module.exports = { transporter, getPasswordResetURL, resetPasswordTemplate }
